d7a92669bf19b03afd4a537f962273a119bba3d2
asifmn <comtechmn@gmail.com>
2021-08-10 06:16:07 -0500

    part 3 without stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,48 +3,74 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
-// ### Required Features
-// Update the `cart.js` file to do the following:
-
 // - Create a global variable named `basket` and set it to an empty array.
+let basket = [];
 
 // - Create a function called `addItem`. It should:
 //   - take an input parameter for a string `item`
 //   - add the new item to the global array `basket`. 
 //   - return `true` indicating the item was added
+function addItem(item) {
+    basket.push(item);
+    if (basket.includes(item)) return true;
+    else return false;
+} // end of function addItem
+
+console.log(addItem('steak'));
+console.log(addItem('potato'));
+
+// log basket array
+console.log('basket: ', basket);
+
+// For example, to test `addItem`:
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
 
 // - Create a function called `listItems`. It should:
-//   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
+
+function listItems() {
+//   - loop over the items in the `basket` array
+    for (let item = 0; item < basket.length; item++) {
+        console.log(basket[item])
+    }
+}
+listItems();
+
 
 // - Create a function called `empty`. It should:
 //   - reset the `basket` to an empty array
 
-// > __IMPORTANT__
-// > Make sure that you are writing code *in the file* to test every function that you write!
+function empty() {
+    basket = [];
+    return basket;
+}
+empty();
+console.log(basket);
 
-// For example, to test `addItem`:
-// ```
-// console.log(`Basket is ${basket}`);
-// console.log('Adding apples (expect true)', addItem('apples'));
-// console.log(`Basket is now ${basket}`);
-// ```
 
 // ### Stretch Goals 
 // Remember that Stretch Goals are not required, but will help you to further develop concepts from the skills we have covered.
 
+
 // __Using functions in other functions!__
 
+
 // 1. Add a global `const` named `maxItems` and set it to 5.
+
 
 // 2. Create a function called isFull(). It should:
 //   - return `false` if the basket contains *less* than max number of items
 //   - return `true` otherwise (equal or more than maxItems)
 
+
 // 3. Update the required `addItem` function to:
 //   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
 //   - If an item was added to the array, return `true`
 //   - If there was no room and the item could not be added return `false`
+
 
 // __Using Array built-in functions!__
 
